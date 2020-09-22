bbfcba1d4f6d76800ec1212b3853cb518bc09f47
Thanos Kapsalis <33357869+thanoskapsalis@users.noreply.github.com>
2020-09-22 16:59:24 +0300

    test

---
+++ TestLoginApp/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestLoginApp.Models;
+
+namespace TestLoginApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public readonly string username = "thanos@gmail.com";
+
+        public readonly string password = "kapsalis";
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult LoginUser(UserModel model)  //opws eipame kai sto ajax ta stoixeia pou 8eloume na steiloume prepei na exoun tin idia  morfi
+        //me to usermodel kai to onoma tou adikeimenoy idio me to seriallized json poy kaname sthn js.
+        {
+            if (model.username == username & model.password == password) 
+                return Json(new {success=true});
+            return Json(new {success = false});
+        }
+    }
+}
---
+++ TestLoginApp/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestLoginApp.Models
+{
+    //Ta Models genika xrhsimevoun gia na stelnoyme dedomena apo tous controller sta views kai to aditheto 
+    //Xrhsimopoioudai toso apo ta ajax request gia na xtisoun ta json oso kai san omi c# mesa sta views gia na organwsoyme ta dedomena mas
+    public class UserModel
+    {
+        public string username { get; set; } //idia morfi me to json
+        public string password { get; set; } //idia morfi me to json
+    }
+}
